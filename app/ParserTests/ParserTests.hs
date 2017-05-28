7b0974e71b2b86615e14504961af627c98bd2e63
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-05-28 19:45:10 -0400

    Give better names for test cases.

--- app/ParserTests/ParserTests.hs
+++ app/ParserTests/ParserTests.hs
@@ -21,24 +21,28 @@ createTest parser (input, expected) = let courseReq =  Parsec.parse parser "" in
 
 
 -- categoryParser Tests
-reqInputs1 :: [(String, Req)]
-reqInputs1 = [("CSC120H1/CSC148H1", OR [J "CSC120H1", J "CSC148H1"])]
-reqInputs2 :: [(String, Req)]
-reqInputs2 = [("CSC120H1, CSC121H1, CSC148H1", AND [J "CSC120H1", J "CSC121H1", J "CSC148H1"])]
-reqInputs3 :: [(String, Req)]
-reqInputs3 = [("CSC148H1/(CSC108H1/CSC120H1, MAT137Y1/MAT157Y1)", OR [J "CSC148H1", AND [OR [J "CSC108H1", J "CSC120H1"], OR [J "MAT137Y1", J "MAT157Y1"]]])]
-reqInputs4 :: [(String, Req)]
-reqInputs4 = [("STA247H1/STA255H1/STA257H1/PSY201H1/ECO227Y1, (MAT135H1, MAT136H1)/MAT137Y1/MAT157Y1", AND [OR [J "STA247H1", J "STA255H1", J "STA257H1", J "PSY201H1", J "ECO227Y1"], OR [AND [J "MAT135H1", J "MAT136H1"], J "MAT137Y1", J "MAT157Y1"]])]
+orInputs :: [(String, Req)]
+orInputs = [("CSC120H1/CSC148H1", OR [J "CSC120H1", J "CSC148H1"])
+		   , ("CSC108H1/CSC120H1/CSC148H1", OR [J "CSC108H1", J "CSC120H1", J "CSC148H1"])]
+andInputs :: [(String, Req)]
+andInputs = [("CSC165H1, CSC236H1", AND [J "CSC165H1", J "CSC236H1"])
+		   , ("CSC120H1, CSC121H1, CSC148H1", AND [J "CSC120H1", J "CSC121H1", J "CSC148H1"])]
+andorInputs :: [(String, Req)]
+andorInputs = [("CSC148H1/CSC207H1, CSC165H1/CSC236H1", AND [OR [J "CSC148H1", J "CSC207H1"], OR [J "CSC165H1", J "CSC236H1"]]) ]
+parInputs :: [(String, Req)]
+parInputs = [("(MAT135H1, MAT136H1)/ MAT137Y1", OR [AND [J "MAT135H1", J "MAT136H1"], J "MAT137Y1"])
+		   , ("CSC148H1/(CSC108H1/CSC120H1, MAT137Y1/MAT157Y1)", OR [J "CSC148H1", AND [OR [J "CSC108H1", J "CSC120H1"], OR [J "MAT137Y1", J "MAT157Y1"]]])
+		   , ("STA247H1/STA255H1/STA257H1/PSY201H1/ECO227Y1, (MAT135H1, MAT136H1)/MAT137Y1/MAT157Y1", AND [OR [J "STA247H1", J "STA255H1", J "STA257H1", J "PSY201H1", J "ECO227Y1"], OR [AND [J "MAT135H1", J "MAT136H1"], J "MAT137Y1", J "MAT157Y1"]])]
 
-reqTests1 :: Test
-reqTests1 = TestLabel "basic categoryParser" $ TestList $ (map (createTest categoryParser) reqInputs1)
-reqTests2 :: Test
-reqTests2 = TestLabel "basic categoryParser" $ TestList $ (map (createTest categoryParser) reqInputs2)
-reqTests3 :: Test
-reqTests3 = TestLabel "basic categoryParser" $ TestList $ (map (createTest categoryParser) reqInputs3)
-reqTests4 :: Test
-reqTests4 = TestLabel "basic categoryParser" $ TestList $ (map (createTest categoryParser) reqInputs4)
+orTests :: Test
+orTests = TestLabel "Basic or Requirement"  $ TestList $ (map (createTest categoryParser) orInputs)
+andTests :: Test
+andTests = TestLabel "Basic and Requirement" $ TestList $ (map (createTest categoryParser) andInputs)
+andorTests :: Test
+andorTests = TestLabel "Basic and-or-mixed Requirement" $ TestList $ (map (createTest categoryParser) andorInputs)
+parTests :: Test
+parTests = TestLabel "Basic and-or-paranthesized Requirement" $ TestList $ (map (createTest categoryParser) parInputs)
 
 -- functions for running tests in REPL
 reqTestSuite :: Test
-reqTestSuite = TestLabel "Req parser tests" $ TestList [reqTests1, reqTests2, reqTests3, reqTests4]
+reqTestSuite = TestLabel "ReqParser tests" $ TestList [orTests, andTests, andorTests, parTests]
